6f98518ce30c43ebdb8b1ee90f95e60027e969b2
Michael Novotny <manovotny@gmail.com>
2016-10-25 21:50:45 -0500

    Adds stylelint-scss plugin.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const stylelint = require('./src/stylelint');
+const scss = require('./src/stylelint');
 
 module.exports = {
+    plugins: [
+        'stylelint-scss'
+    ],
     rules: Object.assign(
         {},
-        stylelint
+        stylelint,
+        scss
     )
 };
---
+++ src/scss.js
@@ -0,0 +1,27 @@
+module.exports = {
+    'scss/at-extend-no-missing-placeholder': true,
+    'scss/at-import-no-partial-leading-underscore': true,
+    'scss/at-import-partial-extension-whitelist': [],
+    'scss/at-mixin-argumentless-call-parentheses': 'never',
+    'scss/declaration-nested-properties': 'never',
+    'scss/dollar-variable-colon-space-after': 'always',
+    'scss/dollar-variable-colon-space-before': 'never',
+    'scss/dollar-variable-no-missing-interpolation': true,
+    'scss/double-slash-comment-empty-line-before': [
+        'always',
+        {
+            except: [
+                'first-nested'
+            ],
+            ignore: [
+                'between-comments',
+                'stylelint-commands'
+            ]
+        }
+    ],
+    'scss/double-slash-comment-whitespace-inside': 'always',
+    'scss/operator-no-newline-after': true,
+    'scss/operator-no-newline-before': true,
+    'scss/operator-no-unspaced': true,
+    'scss/selector-no-redundant-nesting-selector': true
+};
