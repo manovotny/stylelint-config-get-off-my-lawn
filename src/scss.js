6d9863147c074f2112712d289c8575200209e36e
Michael Novotny <manovotny@gmail.com>
2017-07-31 14:33:55 -0500

    Updates stylelint-scss to latest. (#15)

--- src/scss.js
+++ src/scss.js
@@ -3,9 +3,21 @@ module.exports = {
     'scss/at-import-no-partial-leading-underscore': true,
     'scss/at-import-partial-extension-whitelist': [],
     'scss/at-mixin-argumentless-call-parentheses': 'never',
+    'scss/at-rule-no-unknown': true,
     'scss/declaration-nested-properties': 'never',
+    'scss/declaration-nested-properties-no-divided-groups': true,
     'scss/dollar-variable-colon-space-after': 'always',
     'scss/dollar-variable-colon-space-before': 'never',
+    'scss/dollar-variable-empty-line-before': [
+        'always',
+        {
+            except: [
+                'after-comment',
+                'after-dollar-variable',
+                'first-nested'
+            ]
+        }
+    ],
     'scss/dollar-variable-no-missing-interpolation': true,
     'scss/double-slash-comment-empty-line-before': [
         'always',
