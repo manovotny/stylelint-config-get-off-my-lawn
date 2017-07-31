fd2cd4e1df407dd687b6629737c1d0c9887a07e5
Michael Novotny <manovotny@gmail.com>
2017-07-31 15:22:27 -0500

    Removes stylelint-csstree-validator plugin.

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const csstree = require('./src/csstree');
 const order = require('./src/order');
 const scss = require('./src/stylelint');
 const stylelint = require('./src/stylelint');
 
 module.exports = {
     plugins: [
-        'stylelint-csstree-validator',
         'stylelint-order',
         'stylelint-scss'
     ],
     rules: Object.assign(
         {},
-        csstree,
         order,
         scss,
         stylelint
