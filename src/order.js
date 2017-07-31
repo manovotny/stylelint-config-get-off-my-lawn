d97bf72f7a370fd663c02b834905ba9ac2dd9121
Michael Novotny <manovotny@gmail.com>
2017-07-31 14:04:58 -0500

    Relaxes order rules. (#12)

--- src/order.js
+++ src/order.js
@@ -1,10 +1,3 @@
 module.exports = {
-    'order/order': [
-        'custom-properties',
-        'dollar-variables',
-        'rules',
-        'declarations',
-        'at-rules'
-    ],
     'order/properties-alphabetical-order': true
 };
