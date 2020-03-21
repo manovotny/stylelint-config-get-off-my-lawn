56c96a723b4fbdfdcc6debac46eb03534e584cbc
Michael Novotny <manovotny@gmail.com>
2020-03-21 18:02:36 -0500

    Fixes lint errors.

--- src/scss.js
+++ src/scss.js
@@ -4,29 +4,29 @@ module.exports = {
     'scss/at-extend-no-missing-placeholder': true,
     'scss/at-function-named-arguments': 'never',
     'scss/at-function-parentheses-space-before': 'never',
-    'scss/at-mixin-named-arguments': 'never',
-    'scss/at-mixin-parentheses-space-before': 'never',
     'scss/at-if-no-null': true,
     'scss/at-import-no-partial-leading-underscore': true,
     'scss/at-import-partial-extension': 'never',
     'scss/at-mixin-argumentless-call-parentheses': 'never',
+    'scss/at-mixin-named-arguments': 'never',
+    'scss/at-mixin-parentheses-space-before': 'never',
     'scss/at-rule-no-unknown': true,
     'scss/declaration-nested-properties': 'never',
     'scss/declaration-nested-properties-no-divided-groups': true,
     'scss/dimension-no-non-numeric-values': true,
     'scss/dollar-variable-colon-space-after': 'always',
     'scss/dollar-variable-colon-space-before': 'never',
-    'scss/dollar-variable-empty-line-before': [
-        'always',
-        {
-            except: ['after-comment', 'after-dollar-variable', 'first-nested']
-        }
-    ],
     'scss/dollar-variable-empty-line-after': [
         'always',
         {
             except: ['last-nested', 'before-comment', 'before-dollar-variable'],
             ignore: ['before-comment', 'inside-single-line-block']
+        }
+    ],
+    'scss/dollar-variable-empty-line-before': [
+        'always',
+        {
+            except: ['after-comment', 'after-dollar-variable', 'first-nested']
         }
     ],
     'scss/dollar-variable-first-in-block': [
