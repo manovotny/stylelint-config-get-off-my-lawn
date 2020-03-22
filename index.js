4a586845f1b2a3c12c83b6bbf514ccc53ab08d22
Michael Novotny <manovotny@gmail.com>
2020-03-21 20:11:33 -0500

    Adds prettier support.

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+const dotProp = require('dot-prop');
+const readPkgUp = require('read-pkg-up');
+
 const order = require('./src/order');
 const scss = require('./src/stylelint');
 const stylelint = require('./src/stylelint');
 
-module.exports = {
+const pkg = readPkgUp.sync() || {};
+
+const getUsage = (dependency) =>
+    dotProp.get(pkg, `package.dependencies.${dependency}`) || dotProp.get(pkg, `package.devDependencies.${dependency}`);
+
+const config = {
     plugins: ['stylelint-order', 'stylelint-scss'],
-    rules: Object.assign({}, order, scss, stylelint) // eslint-disable-line prefer-object-spread
+    rules: {
+        ...stylelint,
+        ...order,
+        ...scss
+    }
 };
+
+if (getUsage('prettier')) {
+    config.rules = {
+        ...config.rules,
+        ...require('stylelint-config-prettier').rules
+    };
+}
+
+module.exports = config;
