174cec89118831e20fdfa87652a28497c220674a
Michael Novotny <manovotny@gmail.com>
2020-04-02 17:52:42 -0500

    Removes `a11y/media-prefers-color-scheme` as it's annoying to force people to develop for dark mode as it should be a choice.

--- src/a11y.js
+++ src/a11y.js
@@ -1,7 +1,6 @@
 module.exports = {
     'a11y/font-size-is-readable': true,
     'a11y/line-height-is-vertical-rhythmed': true,
-    'a11y/media-prefers-color-scheme': true,
     'a11y/media-prefers-reduced-motion': true,
     'a11y/no-obsolete-attribute': true,
     'a11y/no-obsolete-element': true,
